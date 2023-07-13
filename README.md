80d3c31e1c6ec5f112ae7594e8ee9a033ae7b375
mxstoto6 <mxstotolatomate@gmail.com>
2023-07-13 15:10:33 +0200

    Created repo structure

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # vue-samples
-Vue components to use in a website
+Vue components to use in a website.    
+
+See the components with preview images in [components](./src/Components/).
---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+  import Navbar from './components/transparent-navbar/Navbar.vue'
+</script>
+
+<template>
+  <Navbar />
+  <div class="background"></div>
+</template>
+
+<style scoped>
+  .background {
+        z-index: -1;
+        margin: 0;
+        padding: 0;
+        top:0px;
+        right:0px;
+        bottom:0px;
+        left:0px;
+        background: url(./assets/snowfall-201496.jpg) no-repeat center center fixed; 
+        -webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover;
+        background-size: cover;
+        filter: blur(0px) opacity(0.5);
+        height: 250vh; /*Adjust manually*/
+        position: fixed;
+        min-height: 100%;
+        height: auto;
+    }
+</style>
